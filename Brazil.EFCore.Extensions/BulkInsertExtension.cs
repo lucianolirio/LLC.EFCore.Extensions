00bb9309fd5f69119f240c212d30e47fd1c07115
Luciano Mattos <luciano.lirio@fastslutions.com.br>
2024-01-23 18:45:15 -0300

    .

--- Brazil.EFCore.Extensions/BulkInsertExtension.cs
+++ Brazil.EFCore.Extensions/BulkInsertExtension.cs
@@ -2,238 +2,235 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.EntityFrameworkCore.Storage;
 using System.Data;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
-namespace Brazil.EFCore.Extensions
+namespace Brazil.EFCore.Extensions;
+
+public static class BulkInsertExtension
 {
-    public static class BulkInsertExtension
+    private static readonly object locker = new object();
+
+    internal static IEntityType GetEntityType(this DbContext context, Type type) => context.Model.FindEntityType(type);
+
+    internal static string GetSchema(this DbContext context, Type type) => context.Model.FindEntityType(type)?.GetSchema() ?? "dbo";
+
+    internal static string GetTableName(this DbContext context, Type type) => context.Model.FindEntityType(type).GetTableName();
+
+    internal static string GetFullTableName(this DbContext context, Type type) => $"{context.GetSchema(type)}.{context.GetTableName(type)}";
+
+    internal static IReadOnlyList<IProperty> GetPrimaryKeys(this DbContext context, Type type) => context.GetEntityType(type).FindPrimaryKey().Properties;
+
+    internal static Dictionary<string, IProperty> GetPropertyAnnotations2(this DbContext context, Type type)
     {
-        private static readonly object locker = new object();
+        var result = new Dictionary<string, IProperty>();
 
-        internal static IEntityType GetEntityType(this DbContext context, Type type) => context.Model.FindEntityType(type);
-
-        internal static string GetSchema(this DbContext context, Type type) => context.Model.FindEntityType(type)?.GetSchema() ?? "dbo";
-
-        internal static string GetTableName(this DbContext context, Type type) => context.Model.FindEntityType(type).GetTableName();
-
-        internal static string GetFullTableName(this DbContext context, Type type) => $"{context.GetSchema(type)}.{context.GetTableName(type)}";
-
-        internal static IReadOnlyList<IProperty> GetPrimaryKeys(this DbContext context, Type type) => context.GetEntityType(type).FindPrimaryKey().Properties;
-
-        internal static Dictionary<string, IProperty> GetPropertyAnnotations2(this DbContext context, Type type)
+        var entityType = context.Model.FindEntityType(type);
+        if (entityType != null)
         {
-            var result = new Dictionary<string, IProperty>();
-
-            var entityType = context.Model.FindEntityType(type);
-            if (entityType != null)
+            var properties = entityType.GetProperties();
+            foreach (var property in properties)
             {
-                var properties = entityType.GetProperties();
-                foreach (var property in properties)
-                {
-                    result.Add(property.Name, property);
-                }
+                result.Add(property.Name, property);
             }
-
-            return result;
         }
 
+        return result;
+    }
 
-        internal static List<IProperty> GetProperties(this DbContext context, Type type)
+
+    internal static List<IProperty> GetProperties(this DbContext context, Type type)
+    {
+        var result = new List<IProperty>();
+
+        var entityType = context.Model.FindRuntimeEntityType(type);
+        foreach (var property in entityType.GetProperties())
         {
-            var result = new List<IProperty>();
+            if (property.IsPrimaryKey() && property.ValueGenerated == ValueGenerated.OnAdd)
+                continue;
+            result.Add(property);
+        }
 
-            var entityType = context.Model.FindRuntimeEntityType(type);
+        return result;
+    }
+
+    internal static object GetDiscriminatorValue(this DbContext context, Type type)
+    {
+        return context.Model.FindRuntimeEntityType(type)?
+            .GetAnnotation("Relational:DiscriminatorValue")?.Value;
+    }
+
+    internal static List<IProperty> GetPropertyInfo(this DbContext context, Type type)
+    {
+        var list = context.GetProperties(type);
+        var result = new List<IProperty>();
+
+        foreach (var item in list)
+        {
+            result.Add(item);
+        }
+
+        return result;
+    }
+
+    internal static DataTable CreateTable(this DbContext context, Type type)
+    {
+        var result = new DataTable(context.GetFullTableName(type));
+
+        foreach (var property in context.GetProperties(type))
+        {
+            if (property.IsPrimaryKey() && property.ValueGenerated == ValueGenerated.OnAdd)
+                continue;
+
+            result.Columns.Add(property.Name);
+        }
+
+        //context.GetProperties(type)
+        //    .ForEach(e => result.Columns.Add(
+        //        e.Name,
+        //        Nullable.GetUnderlyingType(e.PropertyInfo.PropertyType) ?? e.PropertyInfo.PropertyType)
+        //    );
+        return result;
+    }
+
+    internal static DataTable GetDataTable<T>(this DbContext context, ICollection<T> list)
+    {
+        var type = list.First().GetType();
+        var entityType = context.Model.FindEntityType(type);
+        var discriminatorProperty = entityType.FindDiscriminatorProperty();
+        var entityTypeName = entityType.GetDiscriminatorPropertyName();
+        var discriminatorPropertyValue = entityType.GetDefaultDiscriminatorValue();
+
+        if (entityType == null)
+            throw new InvalidOperationException("O tipo especificado não é uma entidade conhecida no contexto.");
+
+        var dataTable = new DataTable();
+        foreach (var property in entityType.GetProperties())
+        {
+            dataTable.Columns.Add(property.Name, Nullable.GetUnderlyingType(property.ClrType) ?? property.ClrType);
+        }
+
+        foreach (var entity in list)
+        {
+            var row = dataTable.NewRow();
             foreach (var property in entityType.GetProperties())
             {
-                if (property.IsPrimaryKey() && property.ValueGenerated == ValueGenerated.OnAdd)
-                    continue;
-                result.Add(property);
-            }
-
-            return result;
-        }
-
-        internal static object GetDiscriminatorValue(this DbContext context, Type type)
-        {
-            return context.Model.FindRuntimeEntityType(type)?
-                .GetAnnotation("Relational:DiscriminatorValue")?.Value;
-        }
-
-        internal static List<IProperty> GetPropertyInfo(this DbContext context, Type type)
-        {
-            var list = context.GetProperties(type);
-            var result = new List<IProperty>();
-
-            foreach (var item in list)
-            {
-                result.Add(item);
-            }
-
-            return result;
-        }
-
-        internal static DataTable CreateTable(this DbContext context, Type type)
-        {
-            var result = new DataTable(context.GetFullTableName(type));
-
-            foreach (var property in context.GetProperties(type))
-            {
-                if (property.IsPrimaryKey() && property.ValueGenerated == ValueGenerated.OnAdd)
-                    continue;
-
-                result.Columns.Add(property.Name);
-            }
-
-            //context.GetProperties(type)
-            //    .ForEach(e => result.Columns.Add(
-            //        e.Name,
-            //        Nullable.GetUnderlyingType(e.PropertyInfo.PropertyType) ?? e.PropertyInfo.PropertyType)
-            //    );
-            return result;
-        }
-
-        internal static DataTable GetDataTable<T>(this DbContext context, ICollection<T> list)
-        {
-            var type = list.First().GetType();
-            var entityType = context.Model.FindEntityType(type);
-            var discriminatorProperty = entityType.FindDiscriminatorProperty();
-            var entityTypeName = entityType.GetDiscriminatorPropertyName();
-            var discriminatorPropertyValue = entityType.GetDefaultDiscriminatorValue();
-
-            if (entityType == null)
-                throw new InvalidOperationException("O tipo especificado não é uma entidade conhecida no contexto.");
-
-            var dataTable = new DataTable();
-            foreach (var property in entityType.GetProperties())
-            {
-                dataTable.Columns.Add(property.Name, Nullable.GetUnderlyingType(property.ClrType) ?? property.ClrType);
-            }
-
-            foreach (var entity in list)
-            {
-                var row = dataTable.NewRow();
-                foreach (var property in entityType.GetProperties())
+                if (property.ValueGenerated != ValueGenerated.OnAdd)
                 {
-                    if (property.ValueGenerated != ValueGenerated.OnAdd)
+                    if (discriminatorProperty != null && discriminatorProperty.Equals(property))
                     {
-                        if (discriminatorProperty.Equals(property))
-                        {
-                            row[property.Name] = discriminatorPropertyValue;
-                        }
-                        else
-                        {
-                            var value = property.PropertyInfo.GetValue(entity);
-                            var defaultValue = property.FindAnnotation("Relational:DefaultValue");
-                            row[property.Name] = value ?? defaultValue?.Value ?? DBNull.Value;
-                        }
+                        row[property.Name] = discriminatorPropertyValue;
+                    }
+                    else
+                    {
+                        var value = property.PropertyInfo.GetValue(entity);
+                        var defaultValue = property.FindAnnotation("Relational:DefaultValue");
+                        row[property.Name] = value ?? defaultValue?.Value ?? DBNull.Value;
                     }
                 }
-                dataTable.Rows.Add(row);
             }
-
-            return dataTable;
+            dataTable.Rows.Add(row);
         }
 
-        internal static SqlBulkCopy GetSqlBulkCopy(this DbContext context, IDbContextTransaction transaction)
+        return dataTable;
+    }
+
+    internal static SqlBulkCopy GetSqlBulkCopy(this DbContext context, IDbContextTransaction transaction)
+    {
+        var connecteion = (SqlConnection)context.Database.GetDbConnection();
+
+        var result = new SqlBulkCopy(
+            connecteion,
+            SqlBulkCopyOptions.Default,
+            (SqlTransaction)transaction.GetDbTransaction());
+
+        result.BulkCopyTimeout = connecteion.ConnectionTimeout;
+
+        return result;
+    }
+
+    private static void SetIdentityValue<T>(this DbContext context, ICollection<T> list, IDbContextTransaction transaction)
+    {
+        var pk = context.GetPrimaryKeys(typeof(T)).FirstOrDefault();
+
+        if (pk == null || pk.ValueGenerated != ValueGenerated.OnAdd)
+            return;
+
+        var localList = list.ToList();
+
+        using (var command = context.Database.GetDbConnection().CreateCommand())
         {
-            var connecteion = (SqlConnection)context.Database.GetDbConnection();
+            command.Transaction = (SqlTransaction)transaction.GetDbTransaction();
+            command.CommandText = String.Format("Select top {0} {1} from {2} order by 1 desc", list.Count, pk.Name, context.GetFullTableName(typeof(T)));
 
-            var result = new SqlBulkCopy(
-                connecteion,
-                SqlBulkCopyOptions.Default,
-                (SqlTransaction)transaction.GetDbTransaction());
-
-            result.BulkCopyTimeout = connecteion.ConnectionTimeout;
-
-            return result;
-        }
-
-        private static void SetIdentityValue<T>(this DbContext context, ICollection<T> list, IDbContextTransaction transaction)
-        {
-            var pk = context.GetPrimaryKeys(typeof(T)).FirstOrDefault();
-
-            if (pk == null || pk.ValueGenerated != ValueGenerated.OnAdd)
-                return;
-
-            var localList = list.ToList();
-
-            using (var command = context.Database.GetDbConnection().CreateCommand())
+            using (var reader = command.ExecuteReader())
             {
-                command.Transaction = (SqlTransaction)transaction.GetDbTransaction();
-                command.CommandText = String.Format("Select top {0} {1} from {2} order by 1 desc", list.Count, pk.Name, context.GetFullTableName(typeof(T)));
+                if (!reader.HasRows)
+                    return;
 
-                using (var reader = command.ExecuteReader())
+                var key = typeof(T).GetProperty(pk.Name);
+
+                int i = list.Count;
+                while (reader.Read())
                 {
-                    if (!reader.HasRows)
+                    key.SetValue(localList[--i], reader[pk.Name]);
+                }
+            }
+        }
+    }
+
+    private static void BulkInsertInternal<T>(this DbContext context, ICollection<T> list)
+    {
+        using (var transaction = context.Database.BeginTransaction())
+        {
+            var grupos = (from i in list
+                          group i by i.GetType() into g
+                          select new { Tipo = g.Key, Itens = g.ToList() }).ToList();
+
+            foreach (var item in grupos)
+            {
+                using (var bulkCopy = context.GetSqlBulkCopy(transaction))
+                {
+                    var dataTable = context.GetDataTable(item.Itens);
+
+                    if (dataTable.Columns.Count == 0)
                         return;
 
-                    var key = typeof(T).GetProperty(pk.Name);
+                    bulkCopy.DestinationTableName = context.GetFullTableName(list.First().GetType());
 
-                    int i = list.Count;
-                    while (reader.Read())
+                    foreach (DataColumn column in dataTable.Columns)
                     {
-                        key.SetValue(localList[--i], reader[pk.Name]);
+                        bulkCopy.ColumnMappings.Add(column.ColumnName, column.ColumnName);
                     }
+
+                    bulkCopy.WriteToServer(dataTable);
+                    dataTable.Rows.Clear();
+
+                    context.SetIdentityValue(list, transaction);
+
+                    context.Database.CommitTransaction();
                 }
             }
         }
+    }
 
-        private static void BulkInsertInternal<T>(this DbContext context, ICollection<T> list)
+    public static void BulkInsert<T>(this DbContext context, ICollection<T> list)
+    {
+        lock (locker) { context.BulkInsertInternal(list); }
+    }
+
+    public static void BulkInsert<T>(this DbContext context, ICollection<T> list, int size = int.MaxValue)
+    {
+        lock (locker)
         {
-            using (var transaction = context.Database.BeginTransaction())
+            var group = list
+                .Select((x, i) => new { Index = i, Value = x })
+                .GroupBy(x => x.Index / size).Select(x => x.Select(v => v.Value).ToList())
+                .ToList();
+
+            foreach (var item in group)
             {
-                var grupos = (from i in list
-                              group i by i.GetType() into g
-                              select new { Tipo = g.Key, Itens = g.ToList() }).ToList();
-
-                foreach (var item in grupos)
-                {
-                    using (var bulkCopy = context.GetSqlBulkCopy(transaction))
-                    {
-                        var dataTable = context.GetDataTable(item.Itens);
-
-                        if (dataTable.Columns.Count == 0)
-                            return;
-
-                        bulkCopy.DestinationTableName = context.GetFullTableName(list.First().GetType());
-
-                        foreach (DataColumn column in dataTable.Columns)
-                        {
-                            bulkCopy.ColumnMappings.Add(column.ColumnName, column.ColumnName);
-                        }
-
-                        bulkCopy.WriteToServer(dataTable);
-                        dataTable.Rows.Clear();
-
-                        context.SetIdentityValue(list, transaction);
-
-                        context.Database.CommitTransaction();
-                    }
-                }
-            }
-        }
-
-        public static void BulkInsert<T>(this DbContext context, ICollection<T> list)
-        {
-            lock (locker) { context.BulkInsertInternal(list); }
-        }
-
-        public static void BulkInsert<T>(this DbContext context, ICollection<T> list, int size = int.MaxValue)
-        {
-            lock (locker)
-            {
-                var group = list
-                    .Select((x, i) => new { Index = i, Value = x })
-                    .GroupBy(x => x.Index / size).Select(x => x.Select(v => v.Value).ToList())
-                    .ToList();
-
-                foreach (var item in group)
-                {
-                    context.BulkInsertInternal(item);
-                }
+                context.BulkInsertInternal(item);
             }
         }
     }
--- Brazil.EFCore.Extensions/SelectExtension.cs
+++ Brazil.EFCore.Extensions/SelectExtension.cs
@@ -2,69 +2,68 @@
 using System.Data;
 using System.Reflection;
 
-namespace Brazil.EFCore.Extensions
+namespace Brazil.EFCore.Extensions;
+
+public static class SelectExtension
 {
-    public static class SelectExtension
+    private static Dictionary<string, string> GetFieldNames(this IDataReader reader)
     {
-        private static Dictionary<string, string> GetFieldNames(this IDataReader reader)
+        var result = new Dictionary<string, string>();
+        for (int i = 0; i < reader.FieldCount; i++)
         {
-            var result = new Dictionary<string, string>();
-            for (int i = 0; i < reader.FieldCount; i++)
-            {
-                result.Add(reader.GetName(i), reader.GetName(i));
-            }
-            return result;
+            result.Add(reader.GetName(i), reader.GetName(i));
         }
+        return result;
+    }
 
-        private static List<object> Select(this DbContext context, Type type, string sql, params object[] parameters)
+    private static List<object> Select(this DbContext context, Type type, string sql, params object[] parameters)
+    {
+        var result = new List<object>();
+        var connection = context.Database.GetDbConnection();
+        connection.Open();
+        using (var command = context.Database.GetDbConnection().CreateCommand())
         {
-            var result = new List<object>();
-            var connection = context.Database.GetDbConnection();
-            connection.Open();
-            using (var command = context.Database.GetDbConnection().CreateCommand())
+            command.CommandTimeout = connection.ConnectionTimeout;
+            command.CommandText = sql;
+
+            foreach (var param in parameters) { command.Parameters.Add(param); }
+
+            using (var dataReader = command.ExecuteReader(CommandBehavior.CloseConnection))
             {
-                command.CommandTimeout = connection.ConnectionTimeout;
-                command.CommandText = sql;
+                if (!dataReader.HasRows)
+                    return result;
 
-                foreach (var param in parameters) { command.Parameters.Add(param); }
+                Type entityType = type != null ? type : dataReader.EntityFactoryByDataReader("select");
+                var properties = entityType.GetProperties(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+                var fields = dataReader.GetFieldNames();
 
-                using (var dataReader = command.ExecuteReader(CommandBehavior.CloseConnection))
+                while (dataReader.Read())
                 {
-                    if (!dataReader.HasRows)
-                        return result;
-
-                    Type entityType = type != null ? type : dataReader.EntityFactoryByDataReader("select");
-                    var properties = entityType.GetProperties(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
-                    var fields = dataReader.GetFieldNames();
-
-                    while (dataReader.Read())
+                    var entity = Activator.CreateInstance(entityType);
+                    foreach (var prop in properties)
                     {
-                        var entity = Activator.CreateInstance(entityType);
-                        foreach (var prop in properties)
-                        {
-                            if (!fields.ContainsKey(prop.Name))
-                                continue;
+                        if (!fields.ContainsKey(prop.Name))
+                            continue;
 
-                            if (dataReader[prop.Name].Equals(DBNull.Value))
-                                continue;
+                        if (dataReader[prop.Name].Equals(DBNull.Value))
+                            continue;
 
-                            prop.SetValue(entity, dataReader[prop.Name], null);
-                        }
-                        result.Add(entity);
+                        prop.SetValue(entity, dataReader[prop.Name], null);
                     }
+                    result.Add(entity);
                 }
             }
-            return result;
         }
+        return result;
+    }
 
-        public static List<T> Select<T>(this DbContext context, string sql, params object[] parameters) where T : class, new()
-        {
-            return context.Select(typeof(T), sql, parameters).Cast<T>().ToList();
-        }
+    public static List<T> Select<T>(this DbContext context, string sql, params object[] parameters) where T : class, new()
+    {
+        return context.Select(typeof(T), sql, parameters).Cast<T>().ToList();
+    }
 
-        public static List<object> Select(this DbContext context, string sql, params object[] parameters)
-        {
-            return context.Select(null, sql, parameters);
-        }
+    public static List<object> Select(this DbContext context, string sql, params object[] parameters)
+    {
+        return context.Select(null, sql, parameters);
     }
 }
